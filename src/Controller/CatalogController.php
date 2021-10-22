5ea9f5035bf03fe475d682c599f6c98c88a972fb
Andrew <feshykandrew.work@gmail.com>
2021-10-22 21:40:12 +0300

    add substance form

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Entity\ActiveSubstance;
 
 class CatalogController extends AbstractController
 {
@@ -17,10 +21,31 @@ class CatalogController extends AbstractController
     }
 
     #[Route('/create/substance/', name: 'create_substance')]
-    public function create_substance(): Response
+    public function create_substance(Request $request): Response
     {
-        return $this->render('catalog/create_substance.html.twig', [
-            'controller_action_title' => 'Создание действующего вещества'
+        $activeSubstance = new ActiveSubstance();
+
+        $form = $this->createFormBuilder($activeSubstance)
+            ->add('title', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Добавить'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $activeSubstance = $form->getData();
+
+            // for example, if Task is a Doctrine entity, save it!
+             $entityManager = $this->getDoctrine()->getManager();
+             $entityManager->persist($activeSubstance);
+             $entityManager->flush();
+
+            return $this->redirectToRoute('catalog');
+        }
+
+        return $this->renderForm('catalog/create_substance.html.twig', [
+            'controller_action_title' => 'Добавить действующее вещество',
+            'form' => $form
         ]);
     }
 }
